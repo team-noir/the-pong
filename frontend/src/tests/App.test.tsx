2a2d7da562fa965c1257f09c9cd9e461856ee4d6
hhkim0729 <hhkim0729@gmail.com>
2023-03-09 13:55:13 +0900

    test[FE]: Refactor App, Profile tests
    Co-authored-by: srngch <srngch.dev@gmail.com>

--- frontend/src/tests/App.test.tsx
+++ frontend/src/tests/App.test.tsx
@@ -5,38 +5,57 @@ import '@testing-library/jest-dom';
 import { App, routes } from 'App';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 
-test('메뉴에 있는 페이지들 렌더링', async () => {
-  const route = '/';
+describe('Router - Nav에 있는 페이지들 렌더링', () => {
+  test('Nav의 링크를 클릭하면 각 페이지로 이동한다', async () => {
+    const route = '/';
 
-  const router = await createMemoryRouter(routes(true), {
-    initialEntries: [route],
+    const router = await createMemoryRouter(routes(true), {
+      initialEntries: [route],
+    });
+
+    render(<RouterProvider router={router} />);
+    const user = userEvent.setup();
+
+    screen.getByText('MainPage');
+
+    await waitFor(async () => user.click(screen.getByText(/On Boarding/i)));
+    screen.getByText(/OnBoardingPage/i);
+    await waitFor(async () => user.click(screen.getByText(/game/i)));
+    screen.getByText(/GamePage/i);
+    await waitFor(async () => user.click(screen.getByText(/channel/i)));
+    screen.getByText(/ChannelPage/i);
+    await waitFor(async () => user.click(screen.getByText(/profile/i)));
+    screen.getByText(/ProfilePage/i);
+    await waitFor(async () => user.click(screen.getByText(/setting/i)));
+    screen.getByText(/SettingPage/i);
   });
-
-  render(<RouterProvider router={router} />);
-  const user = userEvent.setup();
-
-  expect(screen.getByText('MainPage')).toBeInTheDocument();
-
-  await waitFor(async () => user.click(screen.getByText(/On Boarding/i)));
-  expect(screen.getByText(/OnBoardingPage/i)).toBeInTheDocument();
-  await waitFor(async () => user.click(screen.getByText(/game/i)));
-  expect(screen.getByText(/GamePage/i)).toBeInTheDocument();
-  await waitFor(async () => user.click(screen.getByText(/channel/i)));
-  expect(screen.getByText(/ChannelPage/i)).toBeInTheDocument();
-  await waitFor(async () => user.click(screen.getByText(/profile/i)));
-  expect(screen.getByText(/ProfilePage/i)).toBeInTheDocument();
-  await waitFor(async () => user.click(screen.getByText(/setting/i)));
-  expect(screen.getByText(/SettingPage/i)).toBeInTheDocument();
 });
 
-test('존재하지 않는 루트에 대한 에러 페이지 렌더링', () => {
-  const badRoute = '/some/bad/route';
+describe('Router - Error Page 렌더링', () => {
+  test('존재하지 않는 경로로 접근할 경우 404 에러 페이지가 렌더링 된다', () => {
+    const badRoute = '/some/bad/route';
 
-  const router = createMemoryRouter(routes(true), {
-    initialEntries: [badRoute],
+    const router = createMemoryRouter(routes(true), {
+      initialEntries: [badRoute],
+    });
+
+    render(<RouterProvider router={router} />);
+
+    screen.getByText(/ErrorPage/i);
   });
+});
 
-  render(<RouterProvider router={router} />);
+describe('Router - ProfilePage 렌더링', () => {
+  test('/profile/:userId로 접근하면 해당 회원의 프로필 페이지를 보여준다', async () => {
+    const route = '/profile/1';
 
-  expect(screen.getByText(/ErrorPage/i)).toBeInTheDocument();
+    const router = createMemoryRouter(routes(true), {
+      initialEntries: [route],
+    });
+
+    render(<RouterProvider router={router} />);
+
+    await screen.findByText('ProfilePage');
+    screen.getByTestId('1');
+  });
 });
