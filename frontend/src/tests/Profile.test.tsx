89696d2f4aac436c7d41942c6155ebd939bef4df
hhkim0729 <hhkim0729@gmail.com>
2023-03-09 19:39:17 +0900

    add[FE]: Add follow button to profile page(#26)
    - Add set profile link to profile page
    
    Co-authored-by: srngch <srngch.dev@gmail.com>

--- frontend/src/tests/Profile.test.tsx
+++ frontend/src/tests/Profile.test.tsx
@@ -1,20 +1,14 @@
-import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { routes } from 'App';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 import Profile from 'components/organisms/Profile';
 
 describe('Component - Profile', () => {
   test('닉네임과 프로필 사진을 보여준다', () => {
-    render(
-      <Profile
-        id="1"
-        nickname="닉네임"
-        profileImageUrl="https://placekitten.com/800/800"
-      />
-    );
-    screen.getByText('닉네임');
+    render(<Profile id="2" />);
+    screen.getByText('닉네임2');
     screen.getByAltText('profile image');
+    screen.getByText('팔로우하기');
   });
 });
 
@@ -29,6 +23,7 @@ describe('Router - ProfilePage', () => {
     render(<RouterProvider router={router} />);
 
     await screen.findByText('ProfilePage');
-    screen.getByTestId('1');
+    await screen.findByTestId('1');
+    await screen.findByText('프로필 수정하기');
   });
 });
