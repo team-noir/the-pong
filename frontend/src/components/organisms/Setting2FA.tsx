5f6d9d2aa49bffec5678a97a9a643b1250b4c121
hhkim0729 <hhkim0729@gmail.com>
2023-03-13 18:53:45 +0900

    add[FE]: Add setting 2FA page(#19)

--- frontend/src/components/organisms/Setting2FA.tsx
+++ frontend/src/components/organisms/Setting2FA.tsx
@@ -1,7 +1,35 @@
+import { useState, useEffect } from 'react';
+import Button from 'components/atoms/Button';
+
+const dummyUser = {
+  id: 1,
+  isTwoFactor: true,
+};
+
 export default function Setting2FA() {
+  const [isTwoFactor, setIsTwoFactor] = useState<boolean>(false);
+
+  useEffect(() => {
+    // TODO: react-query 사용해서 회원 정보 로딩
+    // const user = react-query....
+    setIsTwoFactor(dummyUser.isTwoFactor);
+  }, []);
+
   return (
     <>
       <h1>Setting2FA</h1>
+      <h2>Two-Factor Authentication (2FA)</h2>
+      <p>
+        2단계 보안 인증을 설정하고 계정을 더욱 강력하게 보호하세요.{' '}
+        <a href="">더 알아보기</a>
+      </p>
+      {!isTwoFactor && <Button type="button">설정하기</Button>}
+      {isTwoFactor && (
+        <div>
+          <span>보안 활성화됨</span>
+          <Button type="button">보안 해제하기</Button>
+        </div>
+      )}
     </>
   );
 }
