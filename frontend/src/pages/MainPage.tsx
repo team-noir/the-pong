497a6b594b28917cd525bb55de6f32b7dd1d2129
srngch <srngch.dev@gmail.com>
2023-03-13 19:03:04 +0900

    add[FE]: Add game and channel section to main page
    - Add MainGame component
    - Add MainChannel component

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import MainGames from 'components/organisms/MainGames';
+import MainChannels from 'components/organisms/MainChannels';
 
 export default function MainPage() {
-  return <div>MainPage</div>;
+  return (
+    <>
+      <div>MainPage</div>
+      <MainGames />
+      <MainChannels />
+    </>
+  );
 }
