ef0ac72a35b2706870d96254fe77c87b369ae269
hhkim0729 <hhkim0729@gmail.com>
2023-03-15 17:35:20 +0900

    add[FE]: Apply template components(#111)
    - Modify router setting
    
    Co-authored-by: srngch <srngch.dev@gmail.com>

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -1,51 +1,13 @@
-import { useLogin } from 'hooks/useStore';
-import { Link, Outlet } from 'react-router-dom';
-import SearchBar from 'components/molecule/SearchBar';
-import Button from 'components/atoms/Button';
+import { Outlet } from 'react-router-dom';
+import AppTemplate from 'components/templates/AppTemplate';
+import HeaderGnb from 'components/molecule/HeaderGnb';
 
 export default function Root() {
-  const logout = useLogin((state) => state.logout);
-
-  const handleLogout = () => {
-    logout();
-  };
-
   return (
     <>
-      <nav>
-        <SearchBar />
-        <h5>Nav to test</h5>
-        <li>
-          <Link to="/">Main</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/on-boarding">On Boarding</Link>
-        </li>
-        <li>
-          <Link to="/game">Game</Link>
-        </li>
-        <li>
-          <Link to="/channel">Channel</Link>
-        </li>
-        <li>
-          <Link to="/following">Following</Link>
-        </li>
-        <li>
-          <Link to="/profile/1">Profile</Link>
-        </li>
-        <li>
-          <Link to="/setting">Setting</Link>
-        </li>
-        <Button type="button" onClick={handleLogout}>
-          Logout
-        </Button>
-      </nav>
-      {/* Header */}
-      {/* Footer */}
-      <Outlet />
+      <AppTemplate header={<HeaderGnb />}>
+        <Outlet />
+      </AppTemplate>
     </>
   );
 }
