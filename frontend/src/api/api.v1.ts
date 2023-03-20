4599ea4f535e74c6c158c78946a52b51258c1b84
srngch <srngch.dev@gmail.com>
2023-03-21 01:44:32 +0900

    add[FE]: Add block user api axios and mock functions

--- frontend/src/api/api.v1.ts
+++ frontend/src/api/api.v1.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_PREFIX = `/api/v1`;
+export const API_PREFIX = `/api/v1`;
 export const API_LOGIN_FT = `${API_PREFIX}/auth/42`;
 
 axios.defaults.baseURL = API_PREFIX;
@@ -15,5 +15,21 @@ export const getWhoami = async () => {
 
 export const getHealthCheck = async () => {
   const res = await axios.get(`/health-check`);
+  return res;
+};
+
+export const getMyBlocks = async () => {
+  const res = await axios.get(`/my/blocks`);
+  if (res.status !== 200) {
+    throw new Error(res.statusText);
+  }
+  return res.data;
+};
+
+export const deleteMyBlocks = async (userId: number) => {
+  const res = await axios.delete(`/my/blocks/${userId}`);
+  if (res.status !== 204) {
+    throw new Error(res.statusText);
+  }
   return res;
 };
