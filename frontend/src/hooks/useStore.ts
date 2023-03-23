a7207170de3117f8509c31584d51e5a5c4ccfb11
hhkim0729 <hhkim0729@gmail.com>
2023-03-23 15:39:20 +0900

    add[FE]: Set routes for onboarding page(#183)

--- frontend/src/hooks/useStore.ts
+++ frontend/src/hooks/useStore.ts
@@ -6,8 +6,18 @@ interface LoginState {
   logout: () => void;
 }
 
+interface UserState {
+  hasNickname: boolean;
+  setHasNickname: (hasNickname: boolean) => void;
+}
+
 export const useLogin = create<LoginState>((set) => ({
   isLogin: false,
   login: () => set(() => ({ isLogin: true })),
   logout: () => set(() => ({ isLogin: false })),
+}));
+
+export const useUser = create<UserState>((set) => ({
+  hasNickname: false,
+  setHasNickname: (hasNickname: boolean) => set(() => ({ hasNickname })),
 }));
