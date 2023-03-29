7abf3cbd8e1d2f4700fbec1f2b806f33d9044ca9
PCHANUL <cpak@student.42seoul.kr>
2023-03-29 14:51:15 +0900

    fix[BE]: Fix auth error

--- backend/src/api/channels/channels.module.ts
+++ backend/src/api/channels/channels.module.ts
@@ -2,18 +2,16 @@ import { Module } from '@nestjs/common';
 import { ChannelsGatway } from './channels.gateway';
 import { ChannelsService } from './channels.service';
 import { ChannelsController } from './channels.controller';
-import { AuthService } from '../auth/auth.service';
-import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from '../../prisma/prisma.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  providers: [
-    AuthService,
-    ChannelsGatway,
-    ChannelsService,
-    JwtService,
-    PrismaService,
-  ],
-  controllers: [ChannelsController],
+	imports: [AuthModule],
+	providers: [
+		ChannelsGatway,
+		ChannelsService,
+		PrismaService,
+	],
+	controllers: [ChannelsController],
 })
 export class ChannelsModule {}
