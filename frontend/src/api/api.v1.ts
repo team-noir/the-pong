37a7ba264a2eda52b6d2dd4bc6c8dff272368962
srngch <srngch.dev@gmail.com>
2023-03-31 02:03:41 +0900

    add[FE]: Add api to get messages in channel

--- frontend/src/api/api.v1.ts
+++ frontend/src/api/api.v1.ts
@@ -228,14 +228,6 @@ export const postJoinChannel = async (channelForm: ChannelFormType) => {
   return res;
 };
 
-export const getDmChannel = async (userId: number) => {
-  const res = await axiosWithInterceptors.get(`/dms/${userId}`);
-  if (res.status !== 200) {
-    throw new Error(res.statusText);
-  }
-  return res.data;
-};
-
 export const getChannelMessages = async (channelId: number) => {
   const res = await axiosWithInterceptors.get(`/channels/${channelId}/message`);
   if (res.status !== 200) {
