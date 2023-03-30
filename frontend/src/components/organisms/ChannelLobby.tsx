a55a0cd071acae2147d88859dad83f8c34dbd808
hhkim0729 <hhkim0729@gmail.com>
2023-03-30 18:43:21 +0900

    add[FE]: Add entering already joined channels

--- frontend/src/components/organisms/ChannelLobby.tsx
+++ frontend/src/components/organisms/ChannelLobby.tsx
@@ -1,117 +1,14 @@
 import styles from 'assets/styles/ChannelLobby.module.css';
 import ChannelList from 'components/molecule/ChannelList';
 import { ChannelType } from 'types/channelType';
-import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { RoleType } from 'types/channelUserType';
 
-export const dummyChannels: ChannelType[] = [
-  {
-    id: 1,
-    title: 'public test',
-    isProtected: false,
-    isPrivate: false,
-    isDm: false,
-    users: [
-      {
-        id: 1,
-        nickname: 'heehkim',
-        role: RoleType.owner,
-        isMuted: false,
-      },
-      {
-        id: 2,
-        nickname: 'cpak',
-        role: RoleType.normal,
-        isMuted: false,
-      },
-      {
-        id: 3,
-        nickname: 'sarchoi',
-        role: RoleType.admin,
-        isMuted: false,
-      },
-      {
-        id: 4,
-        nickname: 'hello',
-        role: RoleType.admin,
-        isMuted: false,
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: 'protected test',
-    isProtected: true,
-    isPrivate: false,
-    isDm: false,
-    users: [
-      {
-        id: 3,
-        nickname: 'sarchoi',
-        role: RoleType.owner,
-        isMuted: false,
-      },
-      {
-        id: 4,
-        nickname: 'hello',
-        role: RoleType.normal,
-        isMuted: false,
-      },
-    ],
-  },
-  {
-    id: 3,
-    title: 'private test',
-    isProtected: false,
-    isPrivate: true,
-    isDm: false,
-    users: [
-      {
-        id: 1,
-        nickname: 'heehkim',
-        role: RoleType.owner,
-        isMuted: false,
-      },
-      {
-        id: 4,
-        nickname: 'hello',
-        role: RoleType.normal,
-        isMuted: false,
-      },
-    ],
-  },
-  {
-    id: 4,
-    title: 'dm test',
-    isProtected: false,
-    isPrivate: false,
-    isDm: true,
-    users: [
-      {
-        id: 1,
-        nickname: 'heehkim',
-        role: RoleType.normal,
-        isMuted: false,
-      },
-      {
-        id: 3,
-        nickname: 'sarchoi',
-        role: RoleType.normal,
-        isMuted: false,
-      },
-    ],
-  },
-];
+interface Props {
+  channels: ChannelType[] | null;
+  onClick: (channelId: number) => void;
+}
 
-export default function ChannelLobby() {
-  const [channels, setChannels] = useState<ChannelType[] | null>(null);
-
-  useEffect(() => {
-    // TODO: 현재 유저가 입장한 채널 목록 API에서 가져오기
-    setChannels(dummyChannels);
-  }, []);
-
+export default function ChannelLobby({ channels, onClick }: Props) {
   return (
     <>
       <h1>채널</h1>
@@ -120,7 +17,7 @@ export default function ChannelLobby() {
         <Link to="/channel/new">새 채널 만들기</Link>
       </div>
       <h1>입장 중인 채널 목록</h1>
-      <ChannelList styles={styles} channels={channels} />
+      <ChannelList styles={styles} channels={channels} onClick={onClick} />
     </>
   );
 }
--- frontend/src/tests/ChannelLobby.test.tsx
+++ frontend/src/tests/ChannelLobby.test.tsx
@@ -1,24 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import ChannelLobby from 'components/organisms/ChannelLobby';
+import { dummyChannels } from 'pages/ChannelLobbyPage';
 import { BrowserRouter } from 'react-router-dom';
 
 describe('Component - ChannelLobby 렌더링', () => {
   test('채널 목록을 보여준다', () => {
     render(
       <BrowserRouter>
-        <ChannelLobby />
+        <ChannelLobby
+          channels={dummyChannels}
+          onClick={() => console.log('clicked')}
+        />
       </BrowserRouter>
     );
     screen.getByText('입장 중인 채널 목록');
-  });
-
-  test('더미 데이터 목록을 보여준다', () => {
-    render(
-      <BrowserRouter>
-        <ChannelLobby />
-      </BrowserRouter>
-    );
-    screen.findByText('게임 할 사람');
-    screen.findByText('수다 떨어요');
   });
 });
