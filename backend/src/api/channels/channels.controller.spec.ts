c76b8a8f3eaafe0714d6027d502422a559d3d0b9
PCHANUL <cpak@student.42seoul.kr>
2023-04-01 04:52:37 +0900

    fix[BE]: Fix list

--- backend/src/api/channels/channels.controller.spec.ts
+++ backend/src/api/channels/channels.controller.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UsersController } from './users.controller';
+import { UsersController } from './channels.controller';
 
-describe('UsersController', () => {
+describe('ChannelsController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
