8742ff5b84c0612d0bf251aeabe0f47df3c73ec7
hhkim0729 <hhkim0729@gmail.com>
2023-04-04 17:18:28 +0900

    remove[FE]: Remove unnecessary console.log

--- frontend/src/contexts/socket.ts
+++ frontend/src/contexts/socket.ts
@@ -5,7 +5,6 @@ export const socket = io('localhost:8080', {
   withCredentials: true,
 });
 
-// TODO: 나중에 지우기~! (연결 상태 확인)
-socket.on('connect', () => console.log('socket connected'));
+socket.on('connect', () => console.info('socket connected'));
 
 export const SocketContext = React.createContext(socket);
