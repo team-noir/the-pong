4a8245fb4d2fd26cc477c48437cc0607c660ad6c
srngch <srngch.dev@gmail.com>
2023-04-07 02:08:59 +0900

    refactor[FE]: Modify main css file name and path

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'App';
 import reportWebVitals from 'reportWebVitals';
 import { worker } from 'mocks/browser';
-import 'assets/styles/index.css';
+import 'assets/style.css';
 
 if (process.env.NODE_ENV === 'development') {
   worker.start({
