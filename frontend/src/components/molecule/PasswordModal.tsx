23c57e9ab9ed0e5ec9f542692e1abc9f293d3dc1
srngch <srngch.dev@gmail.com>
2023-04-10 20:13:09 +0900

    add[FE]: Style channel password modal

--- frontend/src/components/molecule/PasswordModal.tsx
+++ frontend/src/components/molecule/PasswordModal.tsx
@@ -1,14 +1,14 @@
 import { useEffect, useRef, useState } from 'react';
+import Modal from 'components/templates/Modal';
 import Button from 'components/atoms/Button';
 import TextInput from 'components/atoms/TextInput';
 
 interface Props {
   onClose: () => void;
   onSubmit: (password: string) => void;
-  children?: React.ReactNode;
 }
 
-export default function PasswordModal({ onClose, onSubmit, children }: Props) {
+export default function PasswordModal({ onClose, onSubmit }: Props) {
   const [password, setPassword] = useState('');
   const modalRef = useRef<HTMLFormElement>(null);
 
@@ -37,18 +37,23 @@ export default function PasswordModal({ onClose, onSubmit, children }: Props) {
   };
 
   return (
-    <form onSubmit={handleSubmit} ref={modalRef}>
-      <Button onClick={onClose}>x</Button>
-      <p>{children}</p>
-      <TextInput
-        type="password"
-        id="password"
-        value={password}
-        onChange={handleChange}
-      />
-      <div>
-        <Button type="submit">확인</Button>
-      </div>
-    </form>
+    <Modal onClickClose={onClose}>
+      <form onSubmit={handleSubmit} ref={modalRef}>
+        <p className="mb-4">비밀번호 입력이 필요한 채널입니다.</p>
+        <TextInput
+          type="password"
+          id="password"
+          placeholder="비밀번호"
+          value={password}
+          onChange={handleChange}
+          fullLength
+        />
+        <div className="mt-4">
+          <Button type="submit" primary fullLength>
+            확인
+          </Button>
+        </div>
+      </form>
+    </Modal>
   );
 }
