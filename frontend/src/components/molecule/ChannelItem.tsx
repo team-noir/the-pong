811200d08bbf87b366419ad20abeca3c9d9d6202
srngch <srngch.dev@gmail.com>
2023-04-10 19:53:29 +0900

    add[FE]: Style cursor in channel list

--- frontend/src/components/molecule/ChannelItem.tsx
+++ frontend/src/components/molecule/ChannelItem.tsx
@@ -9,7 +9,7 @@ export default function Channel({ channel, onClick }: Props) {
   return (
     <li
       onClick={() => onClick && onClick(channel)}
-      className="pl-2 pr-2 pb-4 mb-4 border-b border-b-gray"
+      className="pl-2 pr-2 pb-4 mb-4 border-b border-b-gray cursor-pointer"
     >
       <h5 className="text-xl">{channel.title}</h5>
       {!channel.isDm && <span className="text-sm">{channel.userCount}명</span>}
