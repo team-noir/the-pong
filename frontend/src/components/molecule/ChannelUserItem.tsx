897b06eaa89a6c10163b1f5624fcedf5b702f85b
hhkim0729 <hhkim0729@gmail.com>
2023-04-11 16:22:31 +0900

    fix[FE]: Fix showing channel user button logic

--- frontend/src/components/molecule/ChannelUserItem.tsx
+++ frontend/src/components/molecule/ChannelUserItem.tsx
@@ -26,6 +26,11 @@ export default function ChannelUserItem({
   const queryClient = useQueryClient();
   const isSelf = myUser?.id === user.id;
   const amIOwner = myUser?.role === USER_ROLES.OWNER;
+  const amIAdmin = myUser?.role === USER_ROLES.ADMIN;
+  const isShowSettingButtons =
+    (amIOwner &&
+      (user.role === USER_ROLES.ADMIN || user.role === USER_ROLES.NORMAL)) ||
+    (amIAdmin && user.role === USER_ROLES.NORMAL);
 
   const updateChannelUserRoleMutation = useMutation({
     mutationFn: updateChannelUserRole,
@@ -91,50 +96,49 @@ export default function ChannelUserItem({
             <Button primary size="small">
               게임 초대
             </Button>
-
-            {amIOwner && (
-              <>
-                {user.role === USER_ROLES.ADMIN ? (
-                  <Button onClick={handleClickRole} primary size="small">
-                    관리자 해제
-                  </Button>
-                ) : (
-                  <Button onClick={handleClickRole} primary size="small">
-                    관리자 임명
-                  </Button>
-                )}
-                {user.role !== USER_ROLES.OWNER && (
-                  <>
-                    <Button
-                      value={CHANNEL_USER_STATUS.MUTE}
-                      onClick={handleClickStatus}
-                      secondary
-                      size="small"
-                    >
-                      조용히
-                    </Button>
-                    <Button
-                      value={CHANNEL_USER_STATUS.KICK}
-                      onClick={handleClickStatus}
-                      secondary
-                      size="small"
-                    >
-                      내보내기
-                    </Button>
-                    <Button
-                      value={CHANNEL_USER_STATUS.BAN}
-                      onClick={handleClickStatus}
-                      size="small"
-                      linkStyle
-                      className="text-red"
-                    >
-                      차단하기
-                    </Button>
-                  </>
-                )}
-              </>
-            )}
           </div>
+        )}
+        {amIOwner && (
+          <>
+            {user.role === USER_ROLES.ADMIN ? (
+              <Button onClick={handleClickRole} primary size="small">
+                관리자 해제
+              </Button>
+            ) : (
+              <Button onClick={handleClickRole} primary size="small">
+                관리자 임명
+              </Button>
+            )}
+          </>
+        )}
+        {isShowSettingButtons && (
+          <>
+            <Button
+              value={CHANNEL_USER_STATUS.MUTE}
+              onClick={handleClickStatus}
+              secondary
+              size="small"
+            >
+              조용히
+            </Button>
+            <Button
+              value={CHANNEL_USER_STATUS.KICK}
+              onClick={handleClickStatus}
+              secondary
+              size="small"
+            >
+              내보내기
+            </Button>
+            <Button
+              value={CHANNEL_USER_STATUS.BAN}
+              onClick={handleClickStatus}
+              size="small"
+              linkStyle
+              className="text-red"
+            >
+              차단하기
+            </Button>{' '}
+          </>
         )}
       </div>
     </li>
