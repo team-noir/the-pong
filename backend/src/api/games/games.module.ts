26fd00ab4d54012be75287fbb7a56c6d25b14de2
PCHANUL <cpak@student.42seoul.kr>
2023-04-12 15:39:20 +0900

    fix[BE]: Fix games.module.ts

--- backend/src/api/games/games.module.ts
+++ backend/src/api/games/games.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { ChannelsModule } from '../channels/channels.module';
 import { GamesController } from './games.controller';
 import { GamesService } from './games.service';
+import { AuthModule } from '../auth/auth.module';
+
 
 @Module({
-	imports: [ChannelsModule],
+	imports: [ChannelsModule, AuthModule],
 	providers: [GamesService],
 	controllers: [GamesController],
+	exports: [GamesService]
 })
 export class GamesModule {}
