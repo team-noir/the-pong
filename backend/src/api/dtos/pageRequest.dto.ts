2b927b82e116f05dcd10f2a3ffa4b6a140bf1c19
PCHANUL <cpak@student.42seoul.kr>
2023-05-14 07:07:12 +0900

    add[BE]: Add getPageOptions method

--- backend/src/api/dtos/pageRequest.dto.ts
+++ backend/src/api/dtos/pageRequest.dto.ts
@@ -1,16 +1,49 @@
-import { IsOptional, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsNumberString } from 'class-validator';
 
 export class PageRequestDto {
-	@IsString()
-	@IsOptional()
-	public pageNo?: number | 1;
-
-	@IsString()
+	@ApiProperty({
+		name: 'pageSize',
+		required: false,
+	})
+	@IsNumberString()
 	@IsOptional()
 	public pageSize?: number | 10;
 
+	@ApiProperty({
+		name: 'pageNo',
+		required: false,
+	})
+	@IsNumberString()
+	@IsOptional()
+	public pageNo?: number | 1;
+
+	@ApiProperty({
+		name: 'lastId',
+		required: false,
+	})
+	@IsNumberString()
+	@IsOptional()
+	public lastId?: number;
+
+	getLimit(): number {
+		if (
+			this.pageSize < 1 ||
+			this.pageSize === null ||
+			this.pageSize === undefined
+		) {
+			this.pageSize = 10;
+		}
+		
+		return this.pageSize;
+	}
+
 	getOffset(): number {
-		if (this.pageNo < 1 || this.pageNo === null || this.pageNo === undefined) {
+		if (
+			this.pageNo < 1 || 
+			this.pageNo === null || 
+			this.pageNo === undefined
+		) {
 			this.pageNo = 1;
 		}
 	
@@ -25,15 +58,14 @@ export class PageRequestDto {
 		return (this.pageNo - 1) * this.pageSize;
 	}
 
-	getLimit(): number {
-		if (
-			this.pageSize < 1 ||
-			this.pageSize === null ||
-			this.pageSize === undefined
-		) {
-			this.pageSize = 10;
+	getPageOptions() {
+		return {
+			take: this.getLimit(),
+			skip: this.lastId ? 1 : this.getOffset(),
+			...(this.lastId && {
+				cursor: { id: this.lastId }
+			}),
 		}
-		
-		return this.pageSize;
 	}
+
 }
